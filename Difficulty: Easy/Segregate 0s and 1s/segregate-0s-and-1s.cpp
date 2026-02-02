a4c826ace9bcef44a722e5b0647bbb7d62083dd5
Nirmalya Patra <101016078+nirmalya9@users.noreply.github.com>
2026-02-02 21:50:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -0,0 +1,31 @@
+// User function template for C++
+
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+            int n = arr.size();
+    int count0 = 0;
+    int count1 = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            count0++;
+        }
+        else
+        {
+            count1++;
+        }
+    }
+    for(int i=0;i<count0;i++)
+    {
+        arr[i]=0;
+    }
+    for(int i=count0;i<n;i++)
+    {
+        arr[i]=1;
+    }
+
+    }
+};
